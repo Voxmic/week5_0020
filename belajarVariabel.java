4908e71c32effc81b806437d605bcf4e13c4473b
Voxmic <farelsurya16@gmail.com>
2025-10-28 12:58:41 +0700

    Test

---
+++ belajarVariabel.java
@@ -0,0 +1,15 @@
+public class belajarVariabel {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int angka = 10;
+        String teks = "Halo, Dunia!";
+        double desimal = 5.75;
+        boolean benar = true;
+
+        // Menampilkan nilai variabel
+        System.out.println("Nilai angka: " + angka);
+        System.out.println("Nilai teks: " + teks);
+        System.out.println("Nilai desimal: " + desimal);
+        System.out.println("Nilai benar: " + benar);
+    }
+}
